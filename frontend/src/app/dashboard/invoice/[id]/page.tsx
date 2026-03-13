6bead62df7c6bf6bb2a181349008129ec2504d48
Oyelaja Hussein <premiumitng@gmail.com>
2026-03-13 20:16:21 +0100

    Created dynamic invoice page

---
+++ frontend/src/app/dashboard/invoice/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Invoice Page</h1>
+        <p>This is the invoice page</p>
+        
+    </div>
+  )
+}
+
+export default page
